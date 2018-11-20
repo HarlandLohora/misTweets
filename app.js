3d6950e135d7a3e603f714ca6203a9b95a6b8c0f
tuCantonDevs <harland.lohora@tucanton.com>
2018-11-19 21:49:36 -0600

    ListStyle

--- app.js
+++ app.js
@@ -18,5 +18,20 @@ function tweet(){
 
 function closeModal(){
   let newTweet = document.getElementById('modalTweet');
+  let message = document.getElementById('message').value;
   newTweet.classList.add('noMostrar');
+  message = '';
+}
+
+function newTweet(){
+  let tweets      = document.getElementById('tweets');
+  let message     = document.getElementById('message').value;
+  let tweet       = document.createElement('li');
+  let totalTweets = document.getElementById('numTweets');
+  tweet.classList.add('Tweets');
+  tweet.innerText = message;
+  let numTweets   = document.getElementsByClassName('Tweets');
+  totalTweets.innerText = (numTweets.length)+1;
+  tweets.appendChild(tweet);
+  closeModal();
 }
